6a724e1bbe40babb1a46b6583950dbc7efe5dce8
Carlos d'Avis <carlosdavis@gmail.com>
2013-12-24 18:05:16 -0500

    Specs for document not saving with duplicate-user-id descriptions

--- spec/models/description_spec.rb
+++ spec/models/description_spec.rb
@@ -6,4 +6,13 @@ describe Description do
   it { should belong_to :product }
   it { should belong_to :document }
 
+  context "for a user with existing descriptions" do
+    let!(:user) { User.create }
+    let!(:existing) { Description.create(user_id: user) }
+    it "does not allow a new description added with the same user_id" do
+      description = Description.new(user_id: user)
+      expect(description).to be_invalid
+    end
+  end
+
 end
--- spec/models/document_spec.rb
+++ spec/models/document_spec.rb
@@ -4,4 +4,24 @@ describe Document do
 
   it { should belong_to :user }
 
+  describe "validating associated models" do
+    let!(:user) { User.create }
+    context "for an existing document" do
+      let!(:document) { Document.create }
+      it "does not save if the user has an existing description" do
+        existing = Description.create(user_id: user)
+        new_description = document.descriptions.build(user_id: user)
+        expect(document).to be_invalid
+      end
+    end
+    context "for a new document" do
+      let(:document) { Document.new(user_id: user) }
+      it "does not save if the user has an existing description" do
+        existing = Description.create(user_id: user)
+        new_description = document.descriptions.build(user_id: user)
+        expect(document).to be_invalid
+      end
+    end
+  end
+
 end
